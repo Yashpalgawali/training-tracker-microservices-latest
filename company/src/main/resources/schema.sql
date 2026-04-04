e4190e317fa8ded7d4daaba5aba2a2f71b0d99f1
Yashpal Gawali <gawaliyashpal@gmail.com>
2026-04-04 15:12:39 +0530

    Mysql is added in the same network

--- company/src/main/resources/schema.sql
+++ company/src/main/resources/schema.sql
@@ -1,8 +1,9 @@
-CREATE TABLE IF NOT EXISTS `company` (
-	`company_id` int NOT NUll  ,
-	`company_name` varchar,
-	`created_at` date NOT NULL,
-	`created_by` varchar(20) NOT NULL,
-	`updated_at` date DEFAULT NULL,
-	`updated_by` varchar(20) DEFAULT NULL	 
+CREATE TABLE IF NOT EXISTS company (
+    company_id INT NOT NULL AUTO_INCREMENT,
+    company_name VARCHAR(255) NOT NULL,
+    created_at DATE NOT NULL,
+    created_by VARCHAR(20) NOT NULL,
+    updated_at DATE DEFAULT NULL,
+    updated_by VARCHAR(20) DEFAULT NULL,
+    PRIMARY KEY (company_id)
 );
